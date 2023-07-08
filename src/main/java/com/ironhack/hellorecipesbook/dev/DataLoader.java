28b29c0c5cac46c333561fd86db0aeba8bc27318
salvatorecorsaro <salvatore.corsaro@live.com>
2023-07-08 12:35:58 +0200

    lazy fetch

--- src/main/java/com/ironhack/hellorecipesbook/dev/DataLoader.java
+++ src/main/java/com/ironhack/hellorecipesbook/dev/DataLoader.java
@@ -31,7 +31,7 @@ public class DataLoader implements ApplicationListener<ApplicationReadyEvent> {
 
         // add 4 recipes to each book
         for (int i = 1; i <= 4; i++) {
-            recipesBookService.addRecipeToRecipesBook(book1.getId(), new Recipe("Recipe " + faker.dessert()));
+            recipesBookService.addRecipeToRecipesBook(book1.getId(), new Recipe("Recipe " + faker.dessert().variety()));
             recipesBookService.addRecipeToRecipesBook(book2.getId(), new Recipe("Recipe " + faker.food().fruit()));
             recipesBookService.addRecipeToRecipesBook(book3.getId(), new Recipe("Recipe" + faker.food().dish() ));
         }
